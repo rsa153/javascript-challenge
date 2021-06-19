c881d9304eb0c2c15cd3136c38a28fcf2f26aca3
Rana <r-saber@nlaw.northwestern.edu>
2021-06-19 18:21:41 -0500

    finalize js files

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,16 +5,37 @@ console.log(tableData);
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-tableData.forEach(function(ufoSighting) {
+tableData.forEach(function (ufoSighting) {
     console.log(ufoSighting);
-    // Append one table row `tr` for each UFO Sighting object
     var row = tbody.append("tr");
 
-    // Use `Object.entries` to console.log each UFO Sighting value
-    Object.entries(ufoSighting).forEach(function([key, value]) {
-      console.log(key, value);
-      // Append a cell to the row for each value
-      var cell = row.append("td");
-      cell.text(value);
+    Object.entries(ufoSighting).forEach(function ([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
     });
-  });
+});
+
+var button = d3.select("#filter-btn");
+button.on("click", function () {
+
+    tbody.html("");
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredData);
+
+
+    filteredData.forEach(function (selections) {
+
+        console.log(selections);
+        var row = tbody.append("tr");
+        Object.entries(selections).forEach(function ([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
